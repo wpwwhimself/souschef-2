b9795071c71242ef24d0df2f474dad3ef4e46e30
Wojciech Przybyła <contact@wpww.pl>
2023-09-07 13:54:09 +0200

    categories handling

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    /*****************************
+     * CATEGORIES
+     */
+    public function getCategory($id = null){
+        $data = $id ? Category::find($id) : Category::all();
+        return $data;
+    }
+
+    public function postCategory(Request $rq){
+        $data = Category::create([
+            "name" => $rq->name,
+            "symbol" => $rq->symbol,
+        ]);
+        return $data;
+    }
+
+    public function patchCategory($id, Request $rq){
+        $data = Category::find($id)->update([
+            "name" => $rq->name,
+            "symbol" => $rq->symbol,
+        ]);
+        return $data;
+    }
+
+    public function deleteCategory($id){
+        Category::find($id)->delete();
+        return response("Category deleted");
+    }
+
+    /*****************************
+     * INGREDIENTS
+     */
+
+    /*****************************
+     * PRODUCTS
+     */
+
+    /*****************************
+     * STOCK
+     */
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::controller(ProductController::class)->group(function(){
+    Route::prefix("category")->group(function(){
+        Route::get("/{id?}", "getCategory")->name("get-category");
+        Route::post("/", "postCategory")->name("post-category");
+        Route::patch("/{id}", "patchCategory")->name("patch-category");
+        Route::delete("/{id}", "deleteCategory")->name("delete-category");
+    });
+
+    Route::prefix("product")->group(function(){
+        Route::get("/", "getProducts")->name("get-products");
+    });
 });

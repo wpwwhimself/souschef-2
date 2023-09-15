f59f4890b59d58aeba4c2fa5c89209866e32c458
Wojciech Przybyła <contact@wpww.pl>
2023-09-15 20:07:49 +0200

    api error message corrected?

--- app/Http/Middleware/MagicWord.php
+++ app/Http/Middleware/MagicWord.php
@@ -27,19 +27,13 @@ class MagicWord
             header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
             // treat this as json
             header('Content-Type: application/json');
-            $status = array(
-                200 => '200 OK',
-                400 => '400 Bad Request',
-                422 => 'Unprocessable Entity',
-                500 => '500 Internal Server Error'
-                );
             // ok, validation error, or failure
-            header('Status: '.$status[$code]);
+            header('Status: 401 Unauthorized');
             // return the encoded json
             return json_encode(array(
-                'status' => $code < 300, // success or not?
+                'status' => false, // success or not?
                 'message' => $message
-                ));
+            ));
         }
         return $next($request);
     }

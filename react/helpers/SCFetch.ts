5ff41d065df1849c8054ed089750f2c3848a74dc
Wojciech Przybyła <contact@wpww.pl>
2023-10-21 23:48:13 +0200

    fixing fetching on production

--- react/helpers/SCFetch.ts
+++ react/helpers/SCFetch.ts
@@ -1,39 +1,13 @@
-// import axios from "axios"
+import axios from "axios"
 
 export const rqGet = (URL: string, params?: any) =>
-  // axios.get(URL + "?" + new URLSearchParams(params)).then(res => res.data)
-  fetch(URL + "?" + new URLSearchParams(params))
-    .then(res => res.json())
+  axios.get(URL + "?" + new URLSearchParams(params)).then(res => res.data)
 
 export const rqPost = (URL: string, params?: any) =>
-  // axios.post(URL, params).then(res => res.data)
-  fetch(URL, {
-    method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(params)
-  }).then(res => res.json())
+  axios.post(URL, params).then(res => res.data)
 
 export const rqPatch = (URL: string, params?: any) =>
-  // axios.patch(URL, params).then(res => res.data)
-  fetch(URL, {
-    method: "PATCH",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(params)
-  }).then(res => res.json())
+  axios.patch(URL, params).then(res => res.data)
 
 export const rqDelete = (URL: string, params?: any) =>
-  // axios.delete(URL, params).then(res => res.data)
-  fetch(URL, {
-    method: "DELETE",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(params)
-  }).then(res => res.json())
+  axios.delete(URL, params).then(res => res.data)

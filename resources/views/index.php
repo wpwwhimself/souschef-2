005974bc721c2f18cda94b0f9851e4adfe33209e
Wojciech Przybyła <contact@wpww.pl>
2023-12-05 21:01:01 +0100

    📦 web build

--- resources/views/index.php
+++ resources/views/index.php
@@ -113,5 +113,5 @@
     </noscript>
     <!-- The root element for your Expo app. -->
     <div id="root"></div>
-  <script src="/bundles/web-f54506db7b5a8b95fe6b6a4876e9ccad.js" defer></script></body>
+  <script src="/bundles/web-3ba35c7fd72acacd2d147d81c3d512dc.js" defer></script></body>
 </html>

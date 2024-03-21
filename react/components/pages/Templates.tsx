a6aa5ef1dcac479678535b5c06d873d801a1ed4b
Wojciech Przybyła <contact@wpww.pl>
2024-03-21 22:21:03 +0100

    🗑️ dead templates cleanup

--- react/components/pages/Templates.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from "react-native"
-import Header from "../Header"
-import s from "../../assets/style"
-
-export default function Templates(){
-  return <View style={s.wrapper}>
-    
-  </View>
-}
